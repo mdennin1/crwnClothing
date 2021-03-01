b7921f4da8d0ac3c824a5ded9baa9411678c827f
matthew dennin <danny.dennin@steampunk.com>
2021-02-28 20:03:50 -0500

    updates

--- src/components/menuItem/menuItem.jsx
+++ src/components/menuItem/menuItem.jsx
@@ -3,12 +3,13 @@ import './menuItem.scss';
 
 //functions
 const menuItem = ({imageUrl, size, title}) =>(
-    <div className={`${size} menu-item`} style={{
-        backgroundImage: `url(${imageUrl})`
-    }}>
+    <div className={`${size} menu-item`}>
+        <div className="background-image" style={{
+            backgroundImage: `url(${imageUrl})`
+        }} />
         <div className="content">
             <h1 className="title">
-                {title}
+                {title.toUpperCase()}
             </h1>
             <span className="subtitle">
                 SHOP NOW 
