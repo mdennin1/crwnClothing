8e6e714b352eaf7982b8cb86c3ef476bfd46d002
matthew dennin <danny.dennin@steampunk.com>
2021-03-04 07:28:47 -0500

    login updates

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { userEffect, useState } from 'react';
 import { Link, Route, Router, Switch } from 'react-router-dom';
 //pages
 import HomePage from './pages/homepage/homepage';
@@ -6,8 +6,16 @@ import LoginPage from './pages/loginPage/loginPage';
 import ShopPage from './pages/shopPage/shopPage';
 //components
 import Header from './components/header/header';
+//utils
+import { auth } from './firebase/firebase';
 //
 function App() {
+  const [currentUser, setCurrentUser] = useState(null);
+  userEffect(()=>{
+    auth.onAuthStateChanged(user =>setCurrentUser(user));
+    console.log(currentUser);
+    return (() => auth.onAuthStateChanged(user => setCurrentUser(user)));
+  }, [currentUser]);
   return (
     <div className="App">
       <Header />
